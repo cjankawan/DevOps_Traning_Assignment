78e9e24736c3e753e35e0b64521ec38bdb94b6d7
cjankawan <cjankawan@gmail.com>
2025-04-13 00:12:01 +0545

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express()
+
+const mongoose = require("mongoose");
+
+const { MONGO_USER } = require("./config/config");
+const { MONGO_PASSWORD } = require("./config/config"); 
+const { MONGO_PORT } = require("./config/config"); 
+const { MONGO_IP } = require("./config/config"); 
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        .then(() => console.log("succesfully connected to DB"))
+        .catch((e) => {
+            console.log(e)
+            setTimeout(connectWithRetry, 5000)
+        });
+}
+connectWithRetry();
+// mongoose
+//     // .connect("mongodb://mongo_username:mongo_password@<container-ip-address>:27017/?authSource=admin")
+//     .connect(mongoURL, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => console.log("succesfully connected to DB"))
+//     .catch((e) => console.log(e));
+
+
+app.get("/", (req,res) => {
+    res.send("Hellow === ");
+});
+const port = process.eventNames.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
+
+
